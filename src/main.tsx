f5d142ded8c2364679e16694a86aacfe23290a1d
maitzeth <andre801mz@gmail.com>
2024-04-16 22:44:56 -0300

    feat: adding GA

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from "react-ga4";
 import App from './App.tsx';
 import 'normalize.css';
 import './index.css';
+
+ReactGA.initialize("G-6PCS51B41G");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
